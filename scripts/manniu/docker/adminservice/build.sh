c8e7016d3ba9728692623f9f40c87190d9cc4f11
weichangyu <weichangyu@bullyun.com>
2020-02-23 16:08:08 +0800

    版本改成1.5.1.11

--- scripts/manniu/docker/adminservice/build.sh
+++ scripts/manniu/docker/adminservice/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -f ./Dockerfile -t apollo-adminservice:1.5.1 .
+docker build -f ./Dockerfile -t apollo-adminservice:1.5.1.11 .
 
--- scripts/manniu/docker/configservice/deploy.sh
+++ scripts/manniu/docker/configservice/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker rmi registry.cn-hangzhou.aliyuncs.com/bullyun/apollo-configservice:1.5.1 -f
-docker tag apollo-configservice:1.5.1 registry.cn-hangzhou.aliyuncs.com/bullyun/apollo-configservice:1.5.1
-docker push registry.cn-hangzhou.aliyuncs.com/bullyun/apollo-configservice:1.5.1
+docker rmi registry.cn-hangzhou.aliyuncs.com/bullyun/apollo-configservice:1.5.1.11 -f
+docker tag apollo-configservice:1.5.1.11 registry.cn-hangzhou.aliyuncs.com/bullyun/apollo-configservice:1.5.1.11
+docker push registry.cn-hangzhou.aliyuncs.com/bullyun/apollo-configservice:1.5.1.11
 
